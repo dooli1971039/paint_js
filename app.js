d34cee373e8d83cb82dde17942af10c093d3b240
dooli1971039 <dooli9819@gmail.com>
2022-02-16 05:03:11 +0900

    그림판 js

---
+++ app.js
@@ -0,0 +1,110 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".controls__color"); //배열로 반환
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+//pixel modifier에 사이즈를 줘야한다.
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+//strokeStyle: 선의 색깔
+//fillStyle: 도형의 색깔
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5; //선의 굵기
+
+let painting = false;
+let filling = false;
+
+function startPainting() {
+  painting = true;
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  //offsetX인 이유는, 캔버스 안에서의 위치를 파악하고 싶기 때문
+  //clientX는 전체 창에서의 위치를 의미한다.
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+//event 인터페이스의 target 속성은 이벤트가 발생한 대상 객체를 가리킨다.
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick() {
+  if (filling === true) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+//색 변화시키기
+colors.forEach((color) => color.addEventListener("click", handleColorClick));
+
+//캔버스가 있는지 확인 (없을수도 있으니까)
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove); //마우스의 움직임을 감지하고자 한다.
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+}
+
+//range가 있는지 확인
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+//mode가 있는지 확인
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+/*
+이 방식으로 할 수도 있음
+const colors = document.getElementsByClassName("controls__color");
+
+colors를 배열로 만든 다음에 하나씩 뽑는다.
+위에서 사용한 querySelectorAll은 처음부터 배열로 반환해줘서 Array.form()이 필요 없다.
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
+
+*/
